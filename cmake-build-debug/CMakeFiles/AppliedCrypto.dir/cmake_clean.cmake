4769a5b80f7724dfa8cbe75abb75be155f81667b
Journeyman32 <bryan@coderbox.co>
2021-07-16 19:56:42 -0500

    Initialize Applied Crypto book

---
+++ cmake-build-debug/CMakeFiles/AppliedCrypto.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "AppliedCrypto.exe"
+  "AppliedCrypto.exe.manifest"
+  "AppliedCrypto.lib"
+  "AppliedCrypto.pdb"
+  "CMakeFiles/AppliedCrypto.dir/main.c.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AppliedCrypto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
