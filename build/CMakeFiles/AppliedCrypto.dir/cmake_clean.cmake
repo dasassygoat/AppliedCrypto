011d742d8a5c479f4b107cb76ec21f704188ca60
Journeyman32 <bryan@coderbox.co>
2021-07-19 20:51:52 -0500

    update  makefile on wsl

---
+++ build/CMakeFiles/AppliedCrypto.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "AppliedCrypto"
+  "AppliedCrypto.pdb"
+  "CMakeFiles/AppliedCrypto.dir/xor.c.o"
+  "CMakeFiles/AppliedCrypto.dir/xor.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AppliedCrypto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
