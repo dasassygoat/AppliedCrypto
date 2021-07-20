0a74e775438ac8b7e2c0dd8bd4f97faaea1c195f
Journeyman32 <bryan@coderbox.co>
2021-07-19 19:00:14 -0500

    Added code for  SIMPLE XOR

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+void main(int argc, char *argv[]) {
+    FILE *fi, *fo;
+    char *cp;
+    int c;
+
+    if ((cp = argv[1]) && *cp != '\0') {
+        if ((fi = fopen(argv[2], "rb")) != NULL) {
+
+            while ((c = getc(fi)) != EOF) {
+                if (!*cp) cp = argv[1];
+                c ^= *(cp++);
+                putc(c, fo);
+            }
+        }
+        fclose(fi);
+    }
 }
+
